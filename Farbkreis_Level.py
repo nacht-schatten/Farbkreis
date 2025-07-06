ca1d6dc5153399d3e9a357e6de71630f22a10507
nacht-schatten <joh3011@hotmail.de>
2025-07-06 12:23:21 +0200

    Update Farbkreis_Level.py

--- Farbkreis_Level.py
+++ Farbkreis_Level.py
@@ -34,10 +34,11 @@ if not st.session_state.get("level_bestätigt"):
     }
 
     ausgewähltes_level = st.selectbox(
-         label=None,
-        options=list(level_optionen.keys()),
+        "Level wählen:",
+        list(level_optionen.keys()),
         index=4
     )
+    
 
 
     anzahl_farben, tupel_länge = level_optionen[ausgewähltes_level]
